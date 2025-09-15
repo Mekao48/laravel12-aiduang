a8c8092d53775077c87d25d1dd6ccd30eabb1b82
Mekao48 <84495852+Mekao48@users.noreply.github.com>
2025-09-15 22:01:26 +0700

    database fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(NewsSeeder::class);
+        $this->call(ProductSeeder::class);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
