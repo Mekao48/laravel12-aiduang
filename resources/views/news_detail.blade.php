22b320bbfdbc93c3ef1112e3afe4aa1cda875954
Mekao48 <84495852+Mekao48@users.noreply.github.com>
2025-09-16 13:51:21 +0700

    add date

--- resources/views/news_detail.blade.php
+++ resources/views/news_detail.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-lg-8 mx-auto">
-            <h1 class="mb-3">{{ $news->title }}</h1>
-            <img src="{{ $news->image }}" alt="{{ $news->title }}" class="img-fluid rounded mb-4 shadow-sm">
-            <p class="fs-5">{{ $news->content }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h1 class="mb-3">{{ $news->title }}</h1>
+                <p class="text-muted">
+                    วันที่พิมพ์: {{ \Carbon\Carbon::parse($news->created_at)->format('d M Y H:i') }}
+                </p>
+                <img src="{{ $news->image }}" alt="{{ $news->title }}" class="img-fluid rounded mb-4 shadow-sm">
+                <p class="fs-5">{{ $news->content }}</p>
 
-            <a href="{{ $news->source_url }}" target="_blank" class="btn btn-danger">อ่านจากแหล่งที่มา</a>
-            <a href="{{ route('news') }}" class="btn btn-outline-secondary">ย้อนกลับ</a>
+                <a href="{{ $news->source_url }}" target="_blank" class="btn btn-danger">อ่านจากแหล่งที่มา</a>
+                <a href="{{ route('news') }}" class="btn btn-outline-secondary">ย้อนกลับ</a>
+            </div>
         </div>
     </div>
-</div>
 @endsection
