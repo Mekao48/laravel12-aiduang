0eff16d9eef7d746d0e640d5a1fdd1a36b049477
Mekao48 <84495852+Mekao48@users.noreply.github.com>
2025-09-16 14:28:26 +0700

    fix laravel homepage bug

--- resources/js/pages/settings/appearance.tsx
+++ resources/js/pages/settings/appearance.tsx
@@ -6,11 +6,12 @@ import { type BreadcrumbItem } from '@/types';
 
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
+import { edit as editAppearance } from '@/routes/appearance';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Appearance settings',
-        href: '/settings/appearance',
+        href: editAppearance().url,
     },
 ];
 
